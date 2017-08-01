3e694b7b1621c511de564d1a9f28df5a447a1934
lumiseven <se_ven@192.168.8.103>
2017-08-01 14:39:53 +0800

    hystrix-dashboard

---
+++ hystrix-dashboard/src/main/java/com/seven/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.seven.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
