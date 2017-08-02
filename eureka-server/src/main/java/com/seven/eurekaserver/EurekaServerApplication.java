46bc66b1d76a29ae9e3c5d61985713e976b1af58
lumiseven <se_ven@192.168.8.103>
2017-08-02 18:12:56 +0800

    eureka-server add actuator hystrix support

--- eureka-server/src/main/java/com/seven/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/seven/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.seven.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableCircuitBreaker
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
