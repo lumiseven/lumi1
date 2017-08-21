e1e6a953712ec43a59b3bf6458870fed0381d0d4
lumiseven <se_ven@192.168.0.100>
2017-08-21 18:23:52 +0800

    zuul api-gateway with dynamic filter

---
+++ api-gateway-dynamic-filter/src/main/java/com/seven/apigatewaydynamicfilter/FilterConfiguration.java
@@ -0,0 +1,28 @@
+package com.seven.apigatewaydynamicfilter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("zuul.filter")
+public class FilterConfiguration {
+	
+	private String root;
+	
+	private Integer interval;
+
+	public String getRoot() {
+		return root;
+	}
+
+	public void setRoot(String root) {
+		this.root = root;
+	}
+
+	public Integer getInterval() {
+		return interval;
+	}
+
+	public void setInterval(Integer interval) {
+		this.interval = interval;
+	}
+	
+}
